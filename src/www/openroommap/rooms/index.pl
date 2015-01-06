9750b51804874612ff2ccdd64506c5e3af5baa1e
Andrew Rice <andy@animol.co.uk>
2015-01-06 16:54:04 +0000

    added Apache License

--- src/www/openroommap/rooms/index.pl
+++ src/www/openroommap/rooms/index.pl
@@ -1,3 +1,5 @@
+#!/usr/bin/perl
+
 #*******************************************************************************
 # Copyright 2014 Digital Technology Group, Computer Laboratory
 #
@@ -8,7 +10,7 @@
 # Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
 #*******************************************************************************
-#!/usr/bin/perl
+
 
 use strict;
 use DBI;
